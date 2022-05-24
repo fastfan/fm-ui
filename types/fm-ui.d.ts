ba383724dd781c3b242405ef4ab0b780db6340bd
fanwei <1264682706@qq.com>
2022-05-24 18:32:28 +0800

    feat(*): *

--- types/fm-ui.d.ts
+++ types/fm-ui.d.ts
@@ -2,12 +2,12 @@
  * @Author: fastfan
  * @Date: 2022-05-23 19:59:18
  * @LastEditors: fastfan
- * @LastEditTime: 2022-05-24 15:30:46
+ * @LastEditTime: 2022-05-24 18:31:32
  * @Description: your description
  */
 import Vue from 'vue'
-import { FmCommonDetail } from './common-detail';
-import { FmKeepAlive } from './keep-alive';
+// import { FmCommonDetail } from './common-detail';
+// import { FmKeepAlive } from './keep-alive';
 export interface InstallationOptions {
     router: any,
 }
@@ -16,6 +16,6 @@ export const version: string
 
 export function install(vue: typeof Vue, options: InstallationOptions): void
 
-export class Button extends FmCommonDetail { }
+// export class Button extends FmCommonDetail { }
 
-export class KeepAlive extends FmKeepAlive { }
+// export class KeepAlive extends FmKeepAlive { }
