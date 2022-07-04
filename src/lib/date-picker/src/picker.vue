753491bbf5e3716195867c7f92817085f9db3feb
fanwei <1264682706@qq.com>
2022-07-04 15:40:33 +0800

    fix(*): 修复picker引用路径

--- src/lib/date-picker/src/picker.vue
+++ src/lib/date-picker/src/picker.vue
@@ -87,7 +87,7 @@
 import Vue from 'vue';
 import Clickoutside from 'element-ui/src/utils/clickoutside';
 import { formatDate, parseDate, isDateObject, getWeekNumber } from 'element-ui/src/utils/date-util';
-import Popper from 'element-ui/src/utils/vue-popper';
+import Popper from '../../utils/vue-popper';
 import Emitter from 'element-ui/src/mixins/emitter';
 import ElInput from 'element-ui/packages/input';
 import merge from 'element-ui/src/utils/merge';
